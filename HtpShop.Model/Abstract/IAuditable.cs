bdfccab43c7a7d9e92ccaf29e5ab197d20f43714
Phong Huỳnh <hhtphong1999@gmail.com>
2019-11-18 14:08:24 +0700

    add domain entities for project

---
+++ HtpShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HtpShop.Model.Abstract
+{
+    internal interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+        bool Status { set; get; }
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+    }
+}
